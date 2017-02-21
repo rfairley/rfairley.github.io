97f1e44e63bbef40eea15fdd64d6ca21c5fd049d
Robert Fairley <robertthomasfairley@gmail.com>
2017-02-21 09:06:30 -0700

    Added comments and general refactoring

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,18 @@
-var velocity = 0.4;
+// Define relative speeds of parallax items
+var velocitySplash = 0.4;
 var velocityOverlay = 0.7;
 var velocityBackground = 0.2;
+var numberOfSplashImages = 8;
+
+// Useful properties to store and refer to (avoid hardcoding these values in .js)
 var fixedNavHeight = $('nav.fixed-nav').height();
 var topNavOffset = $('nav.top-nav').height();
-$('nav.fixed-nav').css('top', '-' + fixedNavHeight + 'px');
-var randomNumber =  Math.floor(Math.random() * 8 + 1);
 
+// Random number used for selecting a splash image to display
+var randomNumber =  Math.floor(Math.random() * numberOfSplashImages + 1);
+
+// Initialize arrays containing data for splash image (must be in same order
+// as images are named in the splash folder)
 var splashAlt = [
   'queensferry crossing',
   'king\'s college circle',
@@ -16,7 +23,6 @@ var splashAlt = [
   'lakeside toronto',
   'waterfront seattle'
 ];
-
 var splashCap = [
   'Queensferry Crossing<br>Scotland, 2015',
   'King\'s College Circle<br>Toronto, 2015',
@@ -28,7 +34,13 @@ var splashCap = [
   'Waterfront<br>Seattle, 2016'
 ];
 
+// Initially hide the fixed navigation bar out of the screen
+$('nav.fixed-nav').css('top', '-' + fixedNavHeight + 'px');
+
+// Functions to execute repetitively when the document is ready
 $(document).ready(function() {
+  
+  // Load random cover image in splash box and print to HTML
   $(function () {
     document.getElementById('front-splash-box').innerHTML = '';
     var splashImgHtml = '';
@@ -37,7 +49,8 @@ $(document).ready(function() {
     $('#front-splash-box').append(splashImgHtml);
   });
   
-  $('nav a, .feature h2 a').click(function(){ //slow scrolling to anchors
+  // Slow scrolling to id location when anchors are clicked
+  $('nav a, .feature h2 a').click(function(){
     if ($("html, body").is(":animated")) {
       return;
     }
@@ -48,7 +61,9 @@ $(document).ready(function() {
       $('nav.fixed-nav').css('background-position', '0 0');
     });
   });
-  $(function () { // navbar movements
+  
+  // Define fixed navigation bar movements relative to scrolling
+  $(function () {
     var currentPos = $(window).scrollTop();
     $(document).on('scroll', function() {
       if ($("nav.fixed-nav").is(":animated")) {
@@ -66,11 +81,13 @@ $(document).ready(function() {
       currentPos = pos;
     });
   });
-  $(function () { // parallax
+  
+  // Define parallax behaviour for splash images and textures
+  $(function () {
     $(document).on('scroll', function () {
       var pos = $(window).scrollTop();
       var height = $('#front-splash-box .splash-img').height();
-      $('#front-splash-box').css('height', (height - pos * velocity) + 'px');
+      $('#front-splash-box').css('height', (height - pos * velocitySplash) + 'px');
       $('#front-splash-overlay').css('background-position', '0 ' + ((-pos) * velocityOverlay) + 'px');
       $('#front-splash').css('filter', 'blur(' + (pos * 0.01) + 'px) brightness('+ (1 - pos * 0.0015) + ')');
       $('.feature').css('background-position', '0 ' + ((pos) * velocityBackground) + 'px');
